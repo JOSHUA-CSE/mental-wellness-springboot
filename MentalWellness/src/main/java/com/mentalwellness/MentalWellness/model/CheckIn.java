b8b181069ff09d894502734fee5959f05b51ecbe
Joshua_cse <joshuarubert.r2024cse@sece.ac.in>
2025-10-25 23:31:06 +0530

    refactor:The relationship between user table and checkin table is changed. ->The one to many relation changed to many to one. ->NoArgsConstructor is included to automatically generate the constructor for the class.

--- MentalWellness/src/main/java/com/mentalwellness/MentalWellness/model/CheckIn.java
+++ MentalWellness/src/main/java/com/mentalwellness/MentalWellness/model/CheckIn.java
@@ -1,18 +1,24 @@
 package com.mentalwellness.MentalWellness.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
 public class CheckIn {
         
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long checkin_id;
     
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name="user_id")
     private User user;
     
@@ -21,4 +27,5 @@ public class CheckIn {
     private float sleep_hours;
     private String notes;
     private String checkin_date;
+  
 }
