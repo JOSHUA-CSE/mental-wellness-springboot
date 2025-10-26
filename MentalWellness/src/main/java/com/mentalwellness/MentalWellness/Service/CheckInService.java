a92824394263894ff368a28dda313779c6b49373
Joshua_cse <joshuarubert.r2024cse@sece.ac.in>
2025-10-26 23:50:13 +0530

    refactor:Added the Userrepository object in Checkin service to acces the data in the table. ->coverted optional to list.

--- MentalWellness/src/main/java/com/mentalwellness/MentalWellness/Service/CheckInService.java
+++ MentalWellness/src/main/java/com/mentalwellness/MentalWellness/Service/CheckInService.java
@@ -1,22 +1,36 @@
 package com.mentalwellness.MentalWellness.Service;
 
-import  java.util.Optional;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mentalwellness.MentalWellness.Repository.CheckInRepository;
+import com.mentalwellness.MentalWellness.Repository.UserRepository;
 import com.mentalwellness.MentalWellness.model.CheckIn;
+import com.mentalwellness.MentalWellness.model.User;
 
 @Service
 public class CheckInService {
     @Autowired
     private CheckInRepository checkIn_repo;
+    @Autowired
+    private UserRepository user_repo;
 
-    public Optional<CheckIn> getAllCheckIns(Long id) {
-        return checkIn_repo.findById(id);
+    public List<CheckIn> getAllCheckIns(Long id) {
+        return checkIn_repo.findByUserId(id);
     }
 
-    public CheckIn createCheckIn(CheckIn user) {
-        return checkIn_repo.save(user);
+    public List<CheckIn> getAllCheckIns() {
+        return checkIn_repo.findAll();
+    }
+
+    public CheckIn createCheckIn(CheckIn checkIn) {
+        if (checkIn.getUser()!=null&& checkIn.getUser().getUser_id()!=null) {
+            Long uid=checkIn.getUser().getUser_id();
+            User u=user_repo.findById(uid).orElse(null);
+            checkIn.setUser(u);
+        }
+        return checkIn_repo.save(checkIn);
     }
 }
