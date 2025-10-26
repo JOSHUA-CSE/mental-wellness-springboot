2c212dfe8b8b8bf31bf7c092e2100c0a7ac7c3e5
Joshua_cse <joshuarubert.r2024cse@sece.ac.in>
2025-10-26 23:54:08 +0530

    refactor:Some changes made to handle api URL. ->RestController to controller. ->Userservice object is created to access the elements of user table. ->getAllCheckins() has been added.

--- MentalWellness/src/main/java/com/mentalwellness/MentalWellness/Controller/CheckInController.java
+++ MentalWellness/src/main/java/com/mentalwellness/MentalWellness/Controller/CheckInController.java
@@ -7,33 +7,43 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
 import com.mentalwellness.MentalWellness.Service.CheckInService;
 import com.mentalwellness.MentalWellness.model.CheckIn;
+import com.mentalwellness.MentalWellness.Service.UserService;
 
-@RestController
+@Controller
 @RequestMapping("/api/checkin")
 public class CheckInController {
     @Autowired
     private CheckInService c_service;
+    @Autowired
+    private UserService userService;
     
     @GetMapping("/user/{id}")
     public String getAllCheckIns(@PathVariable Long id, Model model) {
-        model.addAttribute("CheckIn", c_service.getAllCheckIns(id));
-        return "checkIn/list";
+        model.addAttribute("CheckIn",c_service.getAllCheckIns(id));
+        return "checkin/list";
     }
     
     @GetMapping("/new")
     public String createCheckInForm(Model model) {
-        model.addAttribute("checkIn", new CheckIn());
-        return "checkIn/form";
+        model.addAttribute("checkIn",new CheckIn());
+        model.addAttribute("users",userService.getAllUsers());
+        return "checkin/form";
     }
 
     @PostMapping
     public String createCheckIn(@ModelAttribute CheckIn c) {
         c_service.createCheckIn(c);
         return "redirect:/api/checkin";
+    }
+
+    @GetMapping
+    public String getAllCheckIns(Model model) {
+        model.addAttribute("CheckIn",c_service.getAllCheckIns());
+        return "checkin/list";
     }
 
 }
