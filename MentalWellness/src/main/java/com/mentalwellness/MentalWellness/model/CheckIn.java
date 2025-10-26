8d3ec422d0dfc4c979c446f29686b312f574d96b
Joshua_cse <joshuarubert.r2024cse@sece.ac.in>
2025-10-26 23:47:27 +0530

    refactor:The Entities types are edited. ->Primitive to Wrapper class to handle null values effectively. ->string to localdateTime.

--- MentalWellness/src/main/java/com/mentalwellness/MentalWellness/model/CheckIn.java
+++ MentalWellness/src/main/java/com/mentalwellness/MentalWellness/model/CheckIn.java
@@ -1,5 +1,8 @@
 package com.mentalwellness.MentalWellness.model;
 
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,10 +25,13 @@ public class CheckIn {
     @JoinColumn(name="user_id")
     private User user;
     
-    private int mood_level;
-    private int stress_level;
-    private float sleep_hours;
+    private Integer mood_level;
+    private Integer stress_level;
+    private Float sleep_hours;
     private String notes;
-    private String checkin_date;
+
+    @CreationTimestamp
+    @Column(name = "checkin_date", nullable = false)
+    private LocalDateTime checkin_date;
   
 }
