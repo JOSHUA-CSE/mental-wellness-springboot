2ef8a21ac53ac07c4f2482e0de9b127819ca23a3
Joshua_cse <joshuarubert.r2024cse@sece.ac.in>
2025-10-26 23:51:42 +0530

    refactor:The Custom query has benn added to access all users .

--- MentalWellness/src/main/java/com/mentalwellness/MentalWellness/Repository/CheckInRepository.java
+++ MentalWellness/src/main/java/com/mentalwellness/MentalWellness/Repository/CheckInRepository.java
@@ -1,7 +1,16 @@
 package com.mentalwellness.MentalWellness.Repository;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.mentalwellness.MentalWellness.model.CheckIn;
 
 public interface CheckInRepository extends JpaRepository<CheckIn,Long>{
-    
+
+	@Query("SELECT c FROM CheckIn c WHERE c.user.user_id=:userId")
+	List<CheckIn> findByUserId(@Param("userId") Long userId);
+
 }
