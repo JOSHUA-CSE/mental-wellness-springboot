244862beea92303ab7f3efdd6db7920fbc818099
Joshua_cse <joshuarubert.r2024cse@sece.ac.in>
2025-10-26 22:15:25 +0530

    feat:Added a Custom query in Recomendation repository to retieve all the objects with specific id.

--- MentalWellness/src/main/java/com/mentalwellness/MentalWellness/Repository/RecomendationRepository.java
+++ MentalWellness/src/main/java/com/mentalwellness/MentalWellness/Repository/RecomendationRepository.java
@@ -1,7 +1,16 @@
 package com.mentalwellness.MentalWellness.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.mentalwellness.MentalWellness.model.Recomendation;
+
 public interface RecomendationRepository extends JpaRepository<Recomendation,Long>{
+
+	@Query("SELECT r FROM Recomendation r WHERE r.user.user_id = :userId")
+	List<Recomendation> findByUserId(@Param("userId") Long userId);
     
 }
